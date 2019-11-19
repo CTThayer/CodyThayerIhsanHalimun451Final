29411876a7cd197e556ab56c3de24b189c242936
IhsanHalimun <54117660+IhsanHalimun@users.noreply.github.com>
2019-11-19 13:15:01 -0800

    Added SceneNode and NodePrimitive. Added "AddSceneNode()" to SceneNode.

---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Prefab Support/UIPrefab/SceneNodeControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneNodeControl : MonoBehaviour {
+    public SceneNode TheRoot = null;
+
+    const string kChildSpace = "  ";
+    List<Dropdown.OptionData> mSelectMenuOptions = new List<Dropdown.OptionData>();
+    List<Transform> mSelectedTransform = new List<Transform>();    
+
+    // Use this for initialization
+    void Start () {
+        Debug.Assert(TheRoot != null);
+
+        mSelectMenuOptions.Add(new Dropdown.OptionData(TheRoot.transform.name));
+        mSelectedTransform.Add(TheRoot.transform);
+        GetChildrenNames("", TheRoot.transform);
+
+    }
+
+    void GetChildrenNames(string blanks, Transform node)
+    {
+        string space = blanks + kChildSpace;
+        for (int i = node.childCount - 1; i >= 0; i--)
+        {
+            Transform child = node.GetChild(i);
+            SceneNode cn = child.GetComponent<SceneNode>();
+            if (cn != null)
+            {
+                mSelectMenuOptions.Add(new Dropdown.OptionData(space + child.name));
+                mSelectedTransform.Add(child);
+                GetChildrenNames(blanks + kChildSpace, child);
+            }
+        }
+    }
+
+}
