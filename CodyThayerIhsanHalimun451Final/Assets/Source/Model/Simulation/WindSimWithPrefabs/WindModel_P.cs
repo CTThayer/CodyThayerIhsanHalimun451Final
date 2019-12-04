5944b74401b7c532c5ef9e29b5a23607dc51cf48
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-12-04 13:34:08 -0800

    Revert "Wind System Updates"
    This reverts commit 35a65496efba00358b891ecd37765f0a71d143a2.

--- CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/WindModel_P.cs
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/WindModel_P.cs
@@ -4,135 +4,28 @@ using UnityEngine;
 
 public class WindModel_P : MonoBehaviour
 {
-    public Vector3 WindVector;
-    
-    // Gust Interval: user specified Gust time length
-    public float GustInterval;
-
-    public WindNode_P Root;
-
-    //Gust Controller variables
-    private float IntervalEnd;
-    private float IntervalMid;  // Unnecessary?
-    bool newInterval;
-    private int steps;
-
     // Start is called before the first frame update
     void Start()
     {
-        IntervalEnd = 0;
-        IntervalMid = 0;
-        newInterval = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        GustUpdate(Root);
+        
     }
 
-    // New Version
-    public void GustUpdate(WindNode_P n)
+    private void StartWSHelper(WindNode_P Wp)
     {
-        if (newInterval == true)
-        {
-            InitializeInterval();
-            int s = ApproximateStepsPerInterval();
-            SetInitialsHelper(Root, s);
-            newInterval = false;
-        }
-        else if (Time.time < IntervalMid)
-        {
-            // Rotate in "wind direction" 
-            RunWindSim(Root, true);
-            // TODO: Ideally add logic to handle if it hits Gust MAX_R, then rebound slightly
-        }
-        else if (Time.time >= IntervalMid && Time.time < IntervalEnd)
-        {
-            // Reverse rotation direction so that it goes back to initial
-            RunWindSim(Root, false);
-            // TODO: Ideally add logic to handle if it goes past original location
-        }
-        else if (Time.time >= IntervalEnd)
-        {
-            // TODO:  Do we need to ensure node has reset between intervals??
-
-            // Start new interval
-            newInterval = true;
-        }
-    }
-
-
-    private void SetInitialsHelper(WindNode_P Wp, int s)
-    {
-        Wp.SetOriginalRotation(Wp.transform.rotation);
-        Wp.SetStepTheta(s);
-        foreach (Transform child in Wp.transform)
-        {
-            WindNode_P p = child.GetComponent<WindNode_P>();
-            if (p != null)
-            {
-                SetInitialsHelper(p, s);
-            }
-        }
-    }
-
-
-
-    private void RunWindSim(WindNode_P Wp, bool forward)
-    {
-        Quaternion Q = forward ? RunWindSimHelper(Wp) : RunWindSimReverseHelper(Wp);
-        Wp.transform.rotation = Q;
+        
         foreach (Transform child in Wp.transform)
         {
             WindNode_P p = child.GetComponent<WindNode_P> ();
             if (p != null)
             {
-                RunWindSim(p, forward);
+
             }
         }
     }
-
-    private Quaternion RunWindSimHelper(WindNode_P W)
-    {
-        float t = W.GetStepAngle();
-        float Theta = W.ForwardStep(t);
-        Vector3 Axis = W.GetAxis(WindVector);
-        Vector3 Direction = W.GetNodePlusWindDirection(WindVector);
-        Quaternion R = Quaternion.AngleAxis(Theta, Axis);
-
-        return R;
-    }
-
-    private Quaternion RunWindSimReverseHelper(WindNode_P W)
-    {
-        float t = W.GetStepAngle();
-        float Theta = W.BackwardStep(t);
-        Vector3 Axis = W.GetReverseAxis();
-        Vector3 Direction = W.GetNodePlusWindDirection(WindVector);
-        Quaternion R = Quaternion.AngleAxis(Theta, Axis);
-
-        return R;
-    }
-
-
-
-
-    public void InitializeInterval()
-    {
-        // Uncomment to use variance
-        //IntervalEnd = Time.time + GustInterval + (Random.Range(-1.0f, 1.0f) * Variance);
-
-        IntervalEnd = Time.time + GustInterval;
-        IntervalMid = IntervalEnd / 2;
-    }
-
-    public int ApproximateStepsPerInterval()
-    {
-        return (int)(GustInterval / Time.deltaTime);
-    }
-
-
-
-
 }
--- CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/WindNode_P.cs
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/WindNode_P.cs
@@ -6,20 +6,14 @@ public class WindNode_P : MonoBehaviour
 {
     public float MAX_R;
     public float MIN_R;
+    public float CurrentTheta;
     public float Weighting;
 
-    private float CurrentTheta = 0;
-    private float StepTheta = 0;
-
-    
-
     Quaternion OriginalRotation;
-    Vector3 OriginalUp;
     
     public void SetOriginalRotation(Quaternion Q)
     {
         OriginalRotation = Q;
-        OriginalUp = this.transform.up;
     }
 
     public Quaternion GetOriginalRotation()
@@ -34,69 +28,21 @@ public class WindNode_P : MonoBehaviour
     }
 
     // Simple clamping and total rotation tracking
-    public float ForwardStep(float t)
+    public void ForwardStep(float t)
     {
         if (CurrentTheta + t <= MAX_R)
-        {
             CurrentTheta += t;
-            return t;
-        }
         else
-        {
-            float r = MAX_R - CurrentTheta;
             CurrentTheta = MAX_R;
-            return r;
-        }
-          
     }
 
     // Simple clamping and total rotation tracking
-    public float BackwardStep(float t)
+    public void BackwardStep(float t)
     {
         if (CurrentTheta - t >= MIN_R)
-        {
             CurrentTheta -= t;
-            return t;
-        }
         else
-        {
-            float r = CurrentTheta - t;
             CurrentTheta = MIN_R;
-            return r;
-        }
     }
 
-    public Vector3 GetNodePlusWindDirection(Vector3 WindVector)
-    {
-        // Vector direction that the wind pushes the object towards
-        Vector3 CombinedV = this.transform.up + WindVector;
-        return CombinedV.normalized;
-    }
-
-    public float GetStepAngle()
-    {
-        return Weighting * StepTheta;
-    }
-
-    public Vector3 GetAxis(Vector3 vec)
-    {
-        return Vector3.Cross(vec, this.transform.up);
-    }
-
-    public void SetStepTheta(int steps)
-    {
-        StepTheta = MAX_R / steps;
-    }
-
-    public Vector3 GetReverseAxis()
-    {
-        Quaternion backwardR = Quaternion.FromToRotation(transform.up, OriginalUp);
-        float angle;
-        Vector3 axis;
-        backwardR.ToAngleAxis(out angle, out axis);
-
-        //return Vector3.Cross(this.transform.up, OriginalUp);
-
-        return axis;
-    }
 }
