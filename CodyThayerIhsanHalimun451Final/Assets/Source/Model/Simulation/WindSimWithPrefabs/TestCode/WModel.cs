e607f486f9913b9be5b1361bba0415afd5ce0b31
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-12-05 10:45:02 -0800

    Minor changes (have to commit to pull)
    Minor changes (have to commit to pull)

---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WModel : MonoBehaviour
+{
+    public Vector3 WindVector;
+    Vector3 PrevWindVector;
+    WNode Root;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(WindVector )
+    }
+}
---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WNode : MonoBehaviour
+{
+    Vector3 OrigUp;
+    bool forward;
+    float stepsize;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(forward)
+        {
+
+        }
+    }
+
+    void MoveWithWind()
+    {
+
+    }
+
+    void Reverse()
+    {
+        OrigUp
+    }
+}
