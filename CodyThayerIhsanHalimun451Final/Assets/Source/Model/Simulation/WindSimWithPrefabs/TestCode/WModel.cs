09492decea18500d97330a327807f9c06afb115c
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-12-05 17:07:01 -0800

    Working Wind Sim Using "Standard Objects"
    Working Wind Sim Using "Standard Objects" - i.e. NOT using sceneNode and NodePrimitive.
    
    Note: Wind simulation is HIGHLY simplified in this version in order to get a functioning effect.  Complexity can be added from here and tuning will be needed to create realism.

--- CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WModel.cs
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WModel.cs
@@ -5,18 +5,92 @@ using UnityEngine;
 public class WModel : MonoBehaviour
 {
     public Vector3 WindVector;
-    Vector3 PrevWindVector;
-    WNode Root;
-    
+    public WNode Root;
+
+    public GameObject wvHead;
+    public GameObject wvTail;
+    public GameObject wvLine;
+
+    Vector3 PrevHeadLoc;
+    Vector3 PrevTailLoc;
+
+    //Vector3 PrevWindVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(wvHead != null);
+        Debug.Assert(wvTail != null);
+        Debug.Assert(wvLine != null);
+        PrevHeadLoc = wvHead.transform.position;
+        PrevTailLoc = wvTail.transform.position;
+        UpdateWindVector();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(WindVector )
+        // If WindVector controllers have changed, update WindVector
+        if (   wvHead.transform.position != PrevHeadLoc 
+            || wvTail.transform.position != PrevTailLoc  )
+        {
+            UpdateWindVector();
+            PrevHeadLoc = wvHead.transform.position;
+            PrevTailLoc = wvTail.transform.position;
+        }
+
+        // Run UpdateNodes to execute wind simulation
+        UpdateNodes(Root);
+
+
     }
+
+    void UpdateNodes(WNode w)
+    {
+        MoveWNode(w);
+        foreach (Transform child in w.transform)
+        {
+            WNode wn = child.GetComponent<WNode>();
+            if (wn != null)
+            {
+                UpdateNodes(wn);
+            }
+        }
+    }
+
+    void MoveWNode(WNode W)
+    {
+        // Can simplify this if-else
+        if (W.forward && W.currentR < W.MAX_R)
+        {
+            //W.ApplyWindMovement();
+            W.ApplyWindMovementVector(WindVector);
+        }
+        else if (W.forward && W.currentR >= W.MAX_R)
+        {
+            W.forward = false;
+        }
+        else if (!W.forward && W.currentR > W.MIN_R)
+        {
+            //W.ApplyWindMovement();
+            W.ApplyWindMovementVector(WindVector);
+        }
+        else if (!W.forward && W.currentR <= W.MIN_R)
+        {
+            W.forward = true;
+        }
+    }
+
+    private void UpdateWindVector()
+    {
+        WindVector = wvHead.transform.position - wvTail.transform.position;
+
+        wvLine.transform.position = wvTail.transform.position;
+        wvLine.transform.localRotation = Quaternion.FromToRotation(wvLine.transform.up, WindVector);
+        Vector3 scale = wvLine.transform.localScale;
+        scale.y = WindVector.magnitude * 0.5f;
+        wvLine.transform.localScale = scale;
+
+    }
+
 }
--- CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WNode.cs
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/Simulation/WindSimWithPrefabs/TestCode/WNode.cs
@@ -4,32 +4,108 @@ using UnityEngine;
 
 public class WNode : MonoBehaviour
 {
-    Vector3 OrigUp;
-    bool forward;
-    float stepsize;
-    
+
+
+    public float Rigidity;  // Modifier value that impacts how much this node 
+                            // actually moves. 
+                            // 0 = completely rigid, 1 = full rotation allowed
+
+    public float MAX_R;    // Max foward rotation in degrees
+    public float MIN_R;    // Min rotation (Max backward rotation)
+    public float stepsize; // Degrees to rotate per step
+    public bool forward;   // Is movement forward or backward with respect to orig loc
+    public float currentR; // Current degrees rotated
+
+    Vector3 OrigUp; // Original up vector
+
+
     // Start is called before the first frame update
     void Start()
     {
+        OrigUp = this.transform.up;
+    }
+
+    public float GetCurrentR() {return currentR;}
+
+    public void SetOriginalUpVector()
+    {
+        OrigUp = this.transform.up;
+    }
+
+    public Vector3 GetOriginalupVector()
+    {
+        return OrigUp;
+    }
+
+    public void ApplyWindMovement()
+    {
         
-    }
+        // Temp test code
+        Quaternion cR = this.transform.localRotation;
+        Vector3 vecR = cR.eulerAngles;
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        if(forward)
+        if (forward)
         {
-
+            vecR.x += stepsize * Rigidity;
+            currentR += stepsize;
         }
+        else
+        {
+            vecR.x += stepsize * Rigidity * -1.0f;
+            currentR -= stepsize;
+        }
+        
+        this.transform.localRotation = Quaternion.Euler(vecR.x, vecR.y, vecR.z);
+
+
     }
 
-    void MoveWithWind()
+    public void ApplyWindMovementVector(Vector3 WindVector)
     {
 
+        Quaternion result;
+        float theta;
+        Vector3 axis;
+
+        if (forward)
+        {
+            axis = GetForwardAxis(WindVector);
+            theta = stepsize * Rigidity;
+            result = Quaternion.AngleAxis(theta, axis);
+            currentR += stepsize;
+        }
+        else
+        {
+            axis = GetReverseVector();
+            theta = stepsize * Rigidity;
+            result = Quaternion.AngleAxis(theta, axis);
+            currentR -= stepsize;
+        }
+
+        this.transform.localRotation = this.transform.localRotation * result;
+
+
     }
 
-    void Reverse()
+    private Vector3 GetForwardAxis(Vector3 WindVector)
     {
-        OrigUp
+        Vector3 combined = this.transform.up + WindVector;
+        Quaternion Q = Quaternion.FromToRotation(transform.up, combined.normalized);
+
+        float DegToCombined;
+        Vector3 axis;
+        Q.ToAngleAxis(out DegToCombined, out axis);
+        return axis;
     }
+
+    private Vector3 GetReverseVector()
+    {
+        Quaternion Q = Quaternion.FromToRotation(transform.up, OrigUp);
+
+        float DegToCombined;
+        Vector3 axis;
+        Q.ToAngleAxis(out DegToCombined, out axis);
+        return axis;
+    }
+
 }
