14b222e85896a343f9bb38c6d76891ea5007d555
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-12-08 23:35:26 -0800

    SmallCam Controls Added
    SmallCam Controls Added

---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/CameraControllers/SmallCamController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCamController : MonoBehaviour
+{
+    public Camera smallCam;
+    public GameObject target;
+    public float distance;
+    public float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(smallCam != null);
+        Debug.Assert(target != null);
+        Debug.Assert(distance > 0);
+
+        SetToTopView();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void SetToFrontView()
+    {
+        Quaternion Q = Quaternion.AngleAxis(angle, Vector3.right);
+        Vector3 V = Q * Vector3.back;
+
+        // Sets camera position
+        smallCam.transform.position = target.transform.position + (V * distance);
+
+        // Ensures camera looks at the target
+        smallCam.transform.forward = (target.transform.position - smallCam.transform.position).normalized;
+    }
+
+    public void SetToBackView()
+    {
+        Quaternion Q = Quaternion.AngleAxis(angle, Vector3.left);
+        Vector3 V = Q * Vector3.forward;
+
+        // Sets camera position
+        smallCam.transform.position = target.transform.position + (V * distance);
+
+        // Ensures camera looks at the target
+        smallCam.transform.forward = (target.transform.position - smallCam.transform.position).normalized;
+    }
+
+    public void SetToLeftView()
+    {
+        Quaternion Q = Quaternion.AngleAxis(angle, Vector3.forward);
+        Vector3 V = Q * Vector3.right;
+
+        // Sets camera position
+        smallCam.transform.position = target.transform.position + (V * distance);
+
+        // Ensures camera looks at the target
+        smallCam.transform.forward = (target.transform.position - smallCam.transform.position).normalized;
+    }
+
+    public void SetToRightView()
+    {
+        Quaternion Q = Quaternion.AngleAxis(angle, Vector3.back);
+        Vector3 V = Q * Vector3.left;
+
+        // Sets camera position
+        smallCam.transform.position = target.transform.position + (V * distance);
+
+        // Ensures camera looks at the target
+        smallCam.transform.forward = (target.transform.position - smallCam.transform.position).normalized;
+    }
+
+    public void SetToTopView()
+    {
+        // Sets camera position
+        smallCam.transform.position = target.transform.position + (Vector3.up * distance); // * 2.0f);
+
+        // Ensures camera looks at the target
+        smallCam.transform.forward = (target.transform.position - smallCam.transform.position).normalized;
+    }
+
+}
---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Model/UI/SmallCamDropdownController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCamDropdownController : MonoBehaviour
+{
+    public SmallCamController SCC;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(SCC != null);
+    }
+
+    public void SetSmallCamView(int selection)
+    {
+        switch(selection)
+        {
+            case 0:
+                SCC.SetToTopView();
+                break;
+            case 1:
+                SCC.SetToFrontView();
+                break;
+            case 2:
+                SCC.SetToBackView();
+                break;
+            case 3:
+                SCC.SetToRightView();
+                break;
+            case 4:
+                SCC.SetToLeftView();
+                break;
+        }
+    }
+
+}
