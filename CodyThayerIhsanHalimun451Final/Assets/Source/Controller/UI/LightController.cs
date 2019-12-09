b599e34065fb09f4f9db5e3ed3394cf2e2eec4b8
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-12-09 12:09:27 -0800

    UI Setup with Light Controller and Small Cam Frame
    UI Setup with Light Controller and Small Cam Frame
    
    NOTE: TreeNodePrimitive and Collider Hierarchy is completely broken, requires rebuild.

---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Controller/UI/LightController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightController : MonoBehaviour
+{
+    public SliderWithEcho X, Y, Z;
+    public Text ObjectName;
+
+    private Transform mSelected;
+    private Vector3 mPreviousSliderValues = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        X.SetSliderListener(XValueChanged);
+        Y.SetSliderListener(YValueChanged);
+        Z.SetSliderListener(ZValueChanged);
+
+        SetToTranslation(true);
+    }
+
+    //---------------------------------------------------------------------------------
+    // Initialize slider bars to specific function
+    void SetToTranslation(bool v)
+    {
+        Vector3 p = ReadObjectXfrom();
+        mPreviousSliderValues = p;
+        X.InitSliderRange(-2, 2, p.x);
+        Y.InitSliderRange(-2, 2, p.y);
+        Z.InitSliderRange(-2, 2, p.z);
+    }
+
+    //---------------------------------------------------------------------------------
+
+    //---------------------------------------------------------------------------------
+    // resopond to sldier bar value changes
+    void XValueChanged(float v)
+    {
+        Vector3 p = ReadObjectXfrom();
+        // if not in rotation, next two lines of work would be wasted
+        float dx = v - mPreviousSliderValues.x;
+        mPreviousSliderValues.x = v;
+        Quaternion q = Quaternion.AngleAxis(dx, Vector3.right);
+        p.x = v;
+        UISetObjectXform(ref p, ref q);
+    }
+
+    void YValueChanged(float v)
+    {
+        Vector3 p = ReadObjectXfrom();
+        // if not in rotation, next two lines of work would be wasted
+        float dy = v - mPreviousSliderValues.y;
+        mPreviousSliderValues.y = v;
+        Quaternion q = Quaternion.AngleAxis(dy, Vector3.up);
+        p.y = v;
+        UISetObjectXform(ref p, ref q);
+    }
+
+    void ZValueChanged(float v)
+    {
+        Vector3 p = ReadObjectXfrom();
+        // if not in rotation, next two lines of work would be wasterd
+        float dz = v - mPreviousSliderValues.z;
+        mPreviousSliderValues.z = v;
+        Quaternion q = Quaternion.AngleAxis(dz, Vector3.forward);
+        p.z = v;
+        UISetObjectXform(ref p, ref q);
+    }
+    //---------------------------------------------------------------------------------
+
+    // new object selected
+    public void SetSelectedObject(Transform xform)
+    {
+        mSelected = xform;
+        mPreviousSliderValues = Vector3.zero;
+        //if (xform != null)
+        //    ObjectName.text = "Selected:" + xform.name;
+        //else
+        //    ObjectName.text = "Selected: none";
+        ObjectSetUI();
+    }
+
+    public void ObjectSetUI()
+    {
+        Vector3 p = ReadObjectXfrom();
+        X.SetSliderValue(p.x);  // do not need to call back for this comes from the object
+        Y.SetSliderValue(p.y);
+        Z.SetSliderValue(p.z);
+    }
+
+    private Vector3 ReadObjectXfrom()
+    {
+        Vector3 p;
+
+        if (mSelected != null)
+            p = mSelected.localPosition;
+        else
+            p = Vector3.one;
+
+        return p;
+    }
+
+    private void UISetObjectXform(ref Vector3 p, ref Quaternion q)
+    {
+        if (mSelected == null)
+            return;
+
+        mSelected.transform.position = p;
+    }
+
+}
---
+++ CodyThayerIhsanHalimun451Final/Assets/Source/Controller/UI/SetLightXformController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLightXformController : MonoBehaviour
+{
+    public LightController lightController;
+    public GameObject lightObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(lightController != null);
+        Debug.Assert(lightObject != null);
+
+        lightController.SetSelectedObject(lightObject.transform);
+    }
+}
